8794ffd8ad0ad6dfe0506c9c9ad7b541d8cfbf37
jankaz <jk@wp.pl>
2021-03-19 13:34:56 +0100

    Add documentation descriptions to CarUtils methods

--- domain/src/main/java/kazmierczak/jan/CarUtils.java
+++ domain/src/main/java/kazmierczak/jan/CarUtils.java
@@ -8,21 +8,40 @@ import java.util.function.Function;
 import java.util.function.ToIntFunction;
 
 public interface CarUtils {
+    /**
+     * method to compare cars by model
+     */
     Comparator<Car> compareByModel = Comparator.comparing(car -> car.model);
-
+    /**
+     * method to compare cars by mileage
+     */
     Comparator<Car> compareByMileage = Comparator.comparing(car -> car.mileage);
-
+    /**
+     * method to compare cars by price
+     */
     Comparator<Car> compareByPrice = Comparator.comparing(car -> car.price);
-
+    /**
+     * method to compare cars by color
+     */
     Comparator<Car> compareByColor = Comparator.comparing(car -> car.color);
-
+    /**
+     * method to map car object to color of this car
+     */
     Function<Car, Color> toColor = car -> car.color;
-
+    /**
+     * method to map car object to model of this car
+     */
     Function<Car, String> toModel = car -> car.model;
-
+    /**
+     * method to map car object to price of this car
+     */
     Function<Car, BigDecimal> toPrice = car -> car.price;
-
+    /**
+     * method to map car object to mileage of this car
+     */
     ToIntFunction<Car> toMileage = car -> car.mileage;
-
+    /**
+     * method to map car object to price of this car but to get statisitcs
+     */
     org.eclipse.collections.api.block.function.Function<Car, BigDecimal> toPriceStats = car -> car.price;
 }
