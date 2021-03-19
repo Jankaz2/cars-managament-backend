f87daf2b679f7800aa7f39616ae3beb0ad3e3126
jankaz <jk@wp.pl>
2021-03-19 11:59:35 +0100

    Create JsonConverter class to convert any type to json or from json to any type

---
+++ service/src/main/java/kazmierczak/jan/exception/JsonConverterException.java
@@ -0,0 +1,7 @@
+package kazmierczak.jan.exception;
+
+public class JsonConverterException extends RuntimeException{
+    public JsonConverterException(String message) {
+        super(message);
+    }
+}
