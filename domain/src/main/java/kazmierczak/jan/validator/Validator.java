f56b998c1cba8e055200d84c63a3c5f34ec39b48
jankaz <jk@wp.pl>
2021-03-19 11:49:21 +0100

    Create Validator interface

---
+++ domain/src/main/java/kazmierczak/jan/validator/Validator.java
@@ -0,0 +1,24 @@
+package kazmierczak.jan.validator;
+
+
+import kazmierczak.jan.validator.exception.ValidatorException;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public interface Validator<T> {
+    Map<String, String> validate(T item);
+
+    static <T> T validate(Validator<T> validator, T item) {
+        var errors = validator.validate(item);
+        if (!errors.isEmpty()) {
+            var message = errors
+                    .entrySet()
+                    .stream()
+                    .map(e -> e.getKey() + ": " + e.getValue())
+                    .collect(Collectors.joining(", "));
+            throw new ValidatorException("Validation errors: " + message);
+        }
+        return item;
+    }
+}
