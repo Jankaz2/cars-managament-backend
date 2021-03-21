4cdb4b6613c9471d5d39470b1f48db36fcbdc06e
jankaz <jk@wp.pl>
2021-03-21 09:44:35 +0100

    Create test for hasComponent method

---
+++ domain/src/test/java/kazmierczak/jan/HasComponentTest.java
@@ -0,0 +1,21 @@
+package kazmierczak.jan;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HasComponentTest {
+    @Test
+    @DisplayName("returns true if car got component from argument")
+    public void test1() {
+        var component = "WIFI";
+        var car = Car.builder()
+                .components(List.of("GPS", "WIFI")).build();
+        var result = car.hasComponent(component);
+
+        assertThat(result).isTrue();
+    }
+}
