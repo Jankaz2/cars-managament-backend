ac95781b67b4b8bd0b5b61fd095efcf2be36003a
jankaz <jk@wp.pl>
2021-03-22 09:09:52 +0100

    Set context of application in Routin class

--- web/src/main/java/kazmierczak/jan/Routing.java
+++ web/src/main/java/kazmierczak/jan/Routing.java
@@ -1,4 +1,21 @@
 package kazmierczak.jan;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import kazmierczak.jan.config.AppSpringConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@RequiredArgsConstructor
 public class Routing {
+    private CarsService carsService;
+
+    public void initRoutes(){
+        final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        var context = new AnnotationConfigApplicationContext();
+        context.getEnvironment().setActiveProfiles("dev");
+        context.register(AppSpringConfig.class);
+        context.refresh();
+        carsService = context.getBean("carsService", CarsService.class);
+    }
 }
