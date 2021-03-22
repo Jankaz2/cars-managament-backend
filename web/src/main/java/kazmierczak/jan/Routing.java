8e73a3a576e675ede45a86f2bac85f7d3515d540
jankaz <jk@wp.pl>
2021-03-22 09:51:09 +0100

    Create getAllCars and sort routes

--- web/src/main/java/kazmierczak/jan/Routing.java
+++ web/src/main/java/kazmierczak/jan/Routing.java
@@ -4,14 +4,18 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import kazmierczak.jan.config.AppSpringConfig;
 import kazmierczak.jan.filter.CorsFilter;
+import kazmierczak.jan.transformer.JsonTransformer;
+import kazmierczak.jan.types.SortItem;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static spark.Spark.*;
 
 @RequiredArgsConstructor
 public class Routing {
     private CarsService carsService;
 
-    public void initRoutes(){
+    public void initRoutes() {
         final Gson gson = new GsonBuilder().setPrettyPrinting().create();
         var context = new AnnotationConfigApplicationContext();
         context.getEnvironment().setActiveProfiles("dev");
@@ -20,5 +24,25 @@ public class Routing {
         carsService = context.getBean("carsService", CarsService.class);
         var corsFilter = new CorsFilter();
         corsFilter.apply();
+
+        path("/cars", () -> {
+            get("",
+                    (request, response) -> {
+                        response.header("Content-Type", "application/json;charset=utf-8");
+                        return carsService.getAllCars();
+                    }, new JsonTransformer()
+            );
+
+            path("/sort", () -> {
+                get("/:item/:order",
+                        (request, response) -> {
+                            response.header("Content-Type", "application/json;charset=utf-8");
+                            var sortItem = request.params(":item");
+                            var order = request.params(":order");
+                            return carsService.sort(SortItem.valueOf(sortItem), Boolean.parseBoolean(order));
+                        }, new JsonTransformer()
+                );
+            });
+        });
     }
 }
