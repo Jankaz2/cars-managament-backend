a6a654db89afdc2a5285e1b47048798562f62534
jankaz <jk@wp.pl>
2021-04-26 12:49:40 +0200

    Add method to check whether username of user is correct

--- security/src/main/java/kazmierczak/jan/domain/User.java
+++ security/src/main/java/kazmierczak/jan/domain/User.java
@@ -1,5 +1,6 @@
 package kazmierczak.jan.domain;
 
+import kazmierczak.jan.domain.types.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,14 @@ public class User {
     Long id;
     String username;
     String password;
+    private Role role;
+
+    /**
+     * @param username username we are checking
+     * @return true User's username is equal to username from param,
+     * otherwise return false
+     */
+    public boolean hasUsername(String username) {
+        return this.username.equals(username);
+    }
 }
