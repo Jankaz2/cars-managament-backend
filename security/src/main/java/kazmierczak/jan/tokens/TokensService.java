fa3fa500f768f16001f0c74813151ee8a368d28c
jankaz <jk@wp.pl>
2021-04-26 12:42:52 +0200

    Create template of method to generate tokens

--- security/src/main/java/kazmierczak/jan/tokens/TokensService.java
+++ security/src/main/java/kazmierczak/jan/tokens/TokensService.java
@@ -1,8 +1,13 @@
 package kazmierczak.jan.tokens;
 
+import kazmierczak.jan.domain.User;
+import kazmierczak.jan.dto.AuthenticationDataDto;
+import kazmierczak.jan.dto.TokensDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,4 +23,11 @@ public class TokensService {
 
     @Value("${tokens.prefix}")
     private String tokensPrefix;
+
+    public TokensDto generateTokens(AuthenticationDataDto authenticationDataDto) {
+        if (authenticationDataDto == null) {
+            throw new IllegalArgumentException("Authentication data is null");
+        }
+        return null;
+    }
 }
